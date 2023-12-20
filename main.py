4ff229c71603f18c8cac1a158fb09b7406a1b6ed
Anushka Madiwale <116469196+anushkamadiwale04@users.noreply.github.com>
2023-12-20 10:08:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template('index.html')
+    return send_from_directory(os.getcwd(), 'index.html')
 
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
 
 if __name__ == '__main__':
     app.run(debug=True)
